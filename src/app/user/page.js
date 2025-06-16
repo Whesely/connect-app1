fde491dd282bce687c7e078434af0d5853822258
whesely <whesely664@gmail.com>
2025-06-16 21:41:19 +0800

    testt1

---
+++ src/app/user/page.js
@@ -0,0 +1,15 @@
+export default function userpage() {
+  return (
+    <div>
+      <div id="nav">
+        <h1>connect</h1>
+        <div id="list-button">
+          <button>user</button>
+          <button>hak akses</button>
+          <button>logout</button>
+        </div>
+      </div>
+      <div id="konten"></div>
+    </div>
+  );
+}
